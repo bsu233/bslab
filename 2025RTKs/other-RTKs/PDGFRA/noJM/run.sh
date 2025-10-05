efe7c553143a38084992556198a5c2b2232e0026
xhche <155787803+xhche@users.noreply.github.com>
2025-10-05 10:41:47 +0800

    Add files via upload

---
+++ 2025RTKs/other-RTKs/PDGFRA/noJM/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=pdgfra    # Job name
+#SBATCH --ntasks=1                     #
+#SBATCH --ntasks-per-node=1            #
+#SBATCH --cpus-per-task=8              #
+#SBATCH --mem=20gb                     # Job memory request
+#SBATCH --gres=gpu:1                   # GPU request 
+
+
+
+source /public/home/sichao/APPs/gromacs2022/bin/GMXRC
+
+#gmx mdrun -v -deffnm em
+
+#gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr -maxwarn 1
+
+#gmx mdrun -v -deffnm nvt
+
+
+#gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr -maxwarn 1
+
+#gmx mdrun -v -deffnm npt
+
+
+#gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md.tpr
+
+
+#gmx mdrun -deffnm md
+
+gmx convert-tpr -s md.tpr -extend 500000 -o md.tpr
+
+gmx mdrun -v -deffnm md -cpi md.cpt
+
