a9a17f544a56cd4960172c832d3a6e4541332ed7
xhche <155787803+xhche@users.noreply.github.com>
2025-10-07 10:55:37 +0800

    Add files via upload

---
+++ 2025RTKs/other-RTKs/RET/withJM/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=retjm    # Job name
+#SBATCH --ntasks=1                     #
+#SBATCH --ntasks-per-node=1            #
+#SBATCH --cpus-per-task=8              #
+#SBATCH --mem=20gb                     # Job memory request
+#SBATCH --gres=gpu:1                   # GPU request 
+
+source /public/home/sichao/APPs/gromacs2022/bin/GMXRC
+
+
+
+gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr -maxwarn 1
+
+gmx mdrun -v -deffnm nvt
+
+gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr -maxwarn 1
+
+gmx mdrun -v -deffnm npt
+
+gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md.tpr
+
+gmx mdrun -deffnm md
+
